04847c208829b1126ed63f3275d7e74f52b1c594
Matvey Gladkikh <matvey@copters.ru>
2022-12-13 22:00:19 +0300

    db

--- chicha.go
+++ chicha.go
@@ -47,9 +47,10 @@ func main() {
   lap.DiscoveryMinimalUnixTime = 1670935623391
 
 	query := `
-	CREATE TABLE IF NOT EXISTS results(id INTEGER PRIMARY KEY, rider_id TEXT, date BIGINT);
-	INSERT INTO results(rider_id, date) VALUES('TESTRIDER00061453226008', 1670935623391);
-	INSERT INTO results(rider_id, date) VALUES('Volkswagen',21600);
+	CREATE TABLE if not exists Lap(ID INT PRIMARY KEY, SportsmanID INT DEFAULT 0, TagID TEXT, DiscoveryMinimalUnixTime INT DEFAULT 0, DiscoveryAverageUnixTime INT DEFAULT 0, UpdatedAt INT DEFAULT 0, RaceID INT DEFAULT 0, PracticeID INT DEFAULT 0, CurrentRacePosition INT DEFAULT 0, TimeBehindTheLeader INT DEFAULT 0, LapNumber INT DEFAULT 0, LapTime INT DEFAULT 0, LapPosition INT DEFAULT 0, LapIsCurrent BOOL DEFAULT FALSE, LapIsStrange BOOL DEFAULT FALSE, RaceFinished BOOL DEFAULT FALSE, BestLapTime INT DEFAULT 0, BestLapNumber INT DEFAULT 0, BestLapPosition INT DEFAULT 0, RaceTotalTime INT DEFAULT 0, BetterOrWorseLapTime INT DEFAULT 0, UNIQUE(ID));
+	CREATE TABLE if not exists AverageResults(ID INT PRIMARY KEY, LapID INT DEFAULT 0, TagID TEXT, DiscoveryUnixTime INT DEFAULT 0, Antenna INT DEFAULT 0, AntennaIP TEXT DEFAULT 0, UNIQUE(ID));
+
+	INSERT INTO Lap(SportsmanID, DiscoveryAverageUnixTime) VALUES('TESTRIDER00061453226008', 1670935623391);
 	`
 	_, err = db.Exec(query)
 
