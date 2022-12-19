ae0548095f18db1908e017de7632833c3e11b70f
Matvey Gladkikh <matvey@copters.ru>
2022-12-19 14:45:24 +0300

    typo

--- packages/config/config.go
+++ packages/config/config.go
@@ -31,21 +31,22 @@ func init()  {
 	flag.StringVar(&RFID_LISTENER_ADDRESS, "rfid", "0.0.0.0:4000", "Provide IP address and port to listen for RFID data.")
 	flag.StringVar(&WEB_LISTENER_ADDRESS, "web", "0.0.0.0:80", "Provide IP address and port to listen for HTTP connections.")
 	flag.StringVar(&REDIRECT_ADDRESS, "redirect", "", "Redirect rfid data stream to another instance. For example: -redirect '10.9.8.7:4000'.")
-	flag.StringVar(&DB_FILE_PATH, "dbpath", ".", "Provide path to writable directory to store database data.") 
-	flag.StringVar(&DB_TYPE, "dbtype", "sqlite", "Select db type: sqlite / genji / postgres")
 	flag.StringVar(&TIME_ZONE, "timezone", "Europe/London", "Set race timezone.")
-        
-        flag.StringVar(&PG_HOST, "pghost", "127.0.0.1", "PostgreSQL DB host.")
-        flag.IntVar(&PG_PORT, "pgport", 5432, "PostgreSQL DB port.")
-        flag.StringVar(&PG_USER, "pguser", "postgres", "PostgreSQL DB user.")
-        flag.StringVar(&PG_PASS, "pgpass", "", "PostgreSQL DB password.")
-        flag.StringVar(&PG_DB_NAME, "pgdbname", "chicha", "PostgreSQL DB name.")
-        flag.StringVar(&PG_SSL, "pgssl", "prefer", "disable / allow / prefer / require / verify-ca / verify-full - PostgreSQL ssl modes: https://www.postgresql.org/docs/current/libpq-ssl.html")
-
-
 	flag.DurationVar(&RACE_TIMEOUT_DURATION, "timeout", 2*time.Minute, "Set race timeout duration. After this time if nobody passes the finish line the race will be stopped. Valid time units are: 's' (second), 'm' (minute), 'h' (hour).")
 	flag.DurationVar(&MINIMAL_LAP_TIME_DURATION, "laptime", 45*time.Second, "Minimal lap time duration. Results smaller than this duration would be considered wrong." )
 	flag.BoolVar(&AVERAGE_RESULTS, "average", true, "Calculate average results instead of only first results.")
+
+	//db
+	flag.StringVar(&DB_FILE_PATH, "dbpath", ".", "Provide path to writable directory to store database data.")
+	flag.StringVar(&DB_TYPE, "dbtype", "sqlite", "Select db type: sqlite / genji / postgres")
+
+	//PostgreSQL related start
+	flag.StringVar(&PG_HOST, "pghost", "127.0.0.1", "PostgreSQL DB host.")
+	flag.IntVar(&PG_PORT, "pgport", 5432, "PostgreSQL DB port.")
+	flag.StringVar(&PG_USER, "pguser", "postgres", "PostgreSQL DB user.")
+	flag.StringVar(&PG_PASS, "pgpass", "", "PostgreSQL DB password.")
+	flag.StringVar(&PG_DB_NAME, "pgdbname", "chicha", "PostgreSQL DB name.")
+	flag.StringVar(&PG_SSL, "pgssl", "prefer", "disable / allow / prefer / require / verify-ca / verify-full - PostgreSQL ssl modes: https://www.postgresql.org/docs/current/libpq-ssl.html")
 
 	//process all flags
 	flag.Parse()
