94ce5e9065f1619aaaf781bc4492c0af961ba097
Matvey Gladkikh <matvey@copters.ru>
2022-12-23 02:02:20 +0300

    bad worker respawn added (need to fix)

--- packages/proxy/proxy.go
+++ packages/proxy/proxy.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"fmt"
 	"net"
+	"time"
 
 	"chicha/packages/data"
 	"chicha/packages/config"
 )
-
-var proxyWorkersCount = 5
+//оставляем только один процесс который будет брать задачи и передавать их далее на другой сервер (чтобы сохранялась последовательность)
+var proxyWorkersMaxCount = 5
 var proxyTask chan Data.AverageResult
 
 func init() {
@@ -19,34 +20,86 @@ func init() {
 		//initialise channel with tasks:
 		proxyTask = make(chan Data.AverageResult)
 
-		for workerId := 1; workerId <= proxyWorkersCount; workerId++ {
-			go proxyWorkerRun(workerId)
-		}
+
+
+
+		guardChannel := make(chan int, proxyWorkersMaxCount)
+		go func() {
+			for {
+				guardChannel <- 1 // will block if there is proxyWorkersMaxCount ints in guardChannel
+				go func() {
+					go proxyWorkerRun(555)
+					log.Println("Started tcp proxy restream to:", Config.PROXY_ADDRESS)
+					//sleep beafore next worker respawn
+					time.Sleep(5 * time.Second)
+					<-guardChannel // removes an int from guardChannel, allowing another to proceed
+				}()
+
+			}
+		}()
+
+		//for workerId := 1; workerId <= proxyWorkersMaxCount; workerId++ {
+		//	go proxyWorkerRun(workerId)
+		//}
 
 		log.Println("Started tcp proxy restream to:", Config.PROXY_ADDRESS)
 	}
 }
 
+
+
+
 func CreateNewProxyTask(taskData Data.AverageResult) {
-	log.Println("new proxy task:", taskData.TagID)
+	//log.Println("new proxy task:", taskData.TagID)
 	proxyTask <- taskData
 }
 
 func proxyWorkerRun(workerId int) {
-	connection, err := net.Dial("tcp", Config.PROXY_ADDRESS)
+	//connection, err := net.Dial("tcp", Config.PROXY_ADDRESS)
+	connection, err := net.DialTimeout("tcp", Config.PROXY_ADDRESS, 15 * time.Second)
 	if err != nil {
-		log.Printf("worker %d exited due to net.Dial error: %s", workerId, err)
+		log.Printf("Proxy worker %d exited due to net.Dial error: %s\n", workerId, err)
 		return
 	}
 	//close connection on programm exit
 	defer connection.Close()
 
+
+	connectionErrorChannel := make(chan error)
+
+	go func() {
+		buffer := make([]byte, 1024)
+		for {
+			numberOfLines, err := connection.Read(buffer)
+			if err != nil {
+				connectionErrorChannel <- err
+				return
+			}
+			if numberOfLines > 0 {
+				log.Println("Proxy worker received unexpected data back: %s", buffer[:numberOfLines])
+			}
+		}
+	}()
+
 	for {
 		select {
-		case currentProxyTask:= <- proxyTask:
-			fmt.Println("proxyWorker", workerId, "processing new job...")
-			fmt.Fprintf(connection, "%s, %d, %d, %s\n", currentProxyTask.TagID, currentProxyTask.DiscoveryUnixTime, currentProxyTask.Antenna, currentProxyTask.IP)
-			fmt.Println("proxyWorker", workerId, "finished job.")
+			//в случае если есть задание в канале proxyTask
+		case currentProxyTask := <- proxyTask :
+			//fmt.Println("proxyWorker", workerId, "processing new job...")
+			_, networkSendingError := fmt.Fprintf(connection, "%s, %d, %d, %s\n", currentProxyTask.TagID, currentProxyTask.DiscoveryUnixTime, currentProxyTask.Antenna, currentProxyTask.IP)
+			if err != nil {
+				//в случае потери связи во время отправки мы возвращаем задачу обратно в канал proxyTask
+				proxyTask <- currentProxyTask
+				log.Printf("Proxy worker %d exited due to sending error: %s\n", workerId, networkSendingError)
+				//и завершаем работу гоурутины
+				return
+			} else {
+				//fmt.Println("proxyWorker", workerId, "finished job.")
+			}
+		case networkError := <-connectionErrorChannel :
+			//обнаружена сетевая ошибка - завершаем гоурутину
+			log.Printf("Proxy worker %d exited due to connection error: %s\n", workerId, networkError)
+			return
 		}
 	}
 }
