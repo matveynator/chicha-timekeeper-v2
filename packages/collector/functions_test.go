1dc535c8f08264c9826c2aadd12e0f3ab3055fb3
Matvey Gladkikh <matvey@copters.ru>
2023-01-07 11:37:37 +0300

    tests

--- packages/collector/functions_test.go
+++ packages/collector/functions_test.go
@@ -7,33 +7,25 @@ import (
 
 func TestParseCSVLine(t *testing.T) {
 
-	//prepare test data:
-	testInput1 := []byte("100008020200000100000189, 1622570553397, 3")
-	testInput2 := []byte("100008020200000100000189, 1622570553397, 3, 9.9.9.9")
-
-	var testOutput1 Data.RawData
-	testOutput1.TagID="100008020200000100000189"
-	testOutput1.DiscoveryUnixTime=1622570553397
-	testOutput1.ReaderIP = "8.8.8.8"
-	testOutput1.Antenna = 3
-
-	var testOutput2 Data.RawData
-	testOutput2.TagID="100008020200000100000189"
-	testOutput2.DiscoveryUnixTime=1622570553397
-	testOutput2.ReaderIP = "9.9.9.9"
-	testOutput2.Antenna = 3
-  testOutput2.ProxyIP = "8.8.8.8"
-
-
-	//check1:
-	result1, _ := parseCSVLine(testInput1, "8.8.8.8")
-	if testOutput1 != result1 {
-		t.Errorf("Incorrect result. Expect %v, got %v", testOutput1, result1)
+	testData := []struct {
+		csvInput []byte
+		csvOutput Data.RawData
+	}{
+		{
+			csvInput: []byte("100008020200000100000189, 1622570553397, 3"),
+			csvOutput: Data.RawData{ "100008020200000100000189", 1622570553397, "8.8.8.8", 3, "" },
+		},
+		{
+			csvInput: []byte("TONIBOU-001, 1622570553397, 3, 9.9.9.9"),
+			csvOutput: Data.RawData{"TONIBOU-001", 1622570553397, "9.9.9.9", 3, "8.8.8.8" },
+		},
 	}
 
-	//check2:
-	result2, _ := parseCSVLine(testInput2, "8.8.8.8")
-	if testOutput2 != result2 {
-		t.Errorf("Incorrect result. Expect %v, got %v", testOutput2, result2)
+	for _,testCase := range testData {
+		//check1:
+		testResult, _ := parseCSVLine(testCase.csvInput, "8.8.8.8")
+		if testResult != testCase.csvOutput  {
+			t.Errorf("Incorrect result. Expect %v, got %v", testCase.csvOutput, testResult)
+		}
 	}
 }
