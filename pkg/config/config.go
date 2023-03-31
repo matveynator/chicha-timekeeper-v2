c7b87cbde00954ef68d7d529d9b25d52b3999b24
Matvey Gladkikh <matvey@copters.ru>
2023-03-31 17:37:50 +0300

    added database save interval duration to calm down sqlite operations, added database task data return to channel queue in case of database insert failure (to repeat saving later)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,82 +3,83 @@ package Config
 import (
 	"os"
 	"fmt"
-//	"testing"
+	//	"testing"
 	"flag"
 	"time"
 	"hash/fnv"
-	
+
 )
 
-type Settings struct {
-	APP_NAME, VERSION, COLLECTOR_LISTENER_ADDRESS, COLLECTOR_LISTENER_ADDRESS_HASH, WEB_LISTENER_ADDRESS, PROXY_ADDRESS, DB_TYPE, DB_FILE_PATH, DB_FULL_FILE_PATH, PG_HOST, PG_USER, PG_PASS, PG_DB_NAME, PG_SSL, TIME_ZONE string
-	PG_PORT int
-	AVERAGE_RESULTS bool
-	RACE_TIMEOUT_DURATION, MINIMAL_LAP_TIME_DURATION, AVERAGE_DURATION time.Duration
-}
-
-func isFlagPassed(name string) bool {
-	found := false
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	return found
-}
-
-func hash(s string) string {
-	hash := fnv.New32a()
-	hash.Write([]byte(s))
-	return fmt.Sprint(hash.Sum32())
-}
-
-func ParseFlags() (config Settings)  { 
-	config.APP_NAME = "chicha"
-	flagVersion := flag.Bool("version", false, "Output version information")
-	flag.StringVar(&config.COLLECTOR_LISTENER_ADDRESS, "collector", "0.0.0.0:4000", "Provide IP address and port to collect and parse data from RFID and timing readers.")
-	flag.StringVar(&config.WEB_LISTENER_ADDRESS, "web", "0.0.0.0:80", "Provide IP address and port to listen for HTTP connections from clients.")
-	flag.StringVar(&config.PROXY_ADDRESS, "proxy", "", "Proxy received data to another collector. For example: -proxy '10.9.8.7:4000'.")
-	flag.StringVar(&config.TIME_ZONE, "timezone", "UTC", "Set race timezone. Example: Europe/Paris, Africa/Dakar, UTC, https://en.wikipedia.org/wiki/List_of_tz_database_time_zones")
-	flag.DurationVar(&config.RACE_TIMEOUT_DURATION, "timeout", 2*time.Minute, "Set race timeout duration. After this time if nobody passes the finish line the race will be stopped. Valid time units are: 's' (second), 'm' (minute), 'h' (hour).")
-	flag.DurationVar(&config.MINIMAL_LAP_TIME_DURATION, "laptime", 45*time.Second, "Minimal lap time duration. Results smaller than this duration would be considered wrong." )
-	  flag.DurationVar(&config.AVERAGE_DURATION, "average-duration", 1000*time.Millisecond, "Duration to calculate average results. Results passed to reader during this duration will be calculated as average result." )
-	flag.BoolVar(&config.AVERAGE_RESULTS, "average", true, "Calculate average results instead of only first results.")
-
-	//db
-	flag.StringVar(&config.DB_FILE_PATH, "dbpath", ".", "Provide path to writable directory to store database data.")
-	flag.StringVar(&config.DB_TYPE, "dbtype", "sqlite", "Select db type: sqlite / genji / postgres")
-
-	//PostgreSQL related start
-	flag.StringVar(&config.PG_HOST, "pghost", "127.0.0.1", "PostgreSQL DB host.")
-	flag.IntVar(&config.PG_PORT, "pgport", 5432, "PostgreSQL DB port.")
-	flag.StringVar(&config.PG_USER, "pguser", "postgres", "PostgreSQL DB user.")
-	flag.StringVar(&config.PG_PASS, "pgpass", "", "PostgreSQL DB password.")
-	flag.StringVar(&config.PG_DB_NAME, "pgdbname", "chicha", "PostgreSQL DB name.")
-	flag.StringVar(&config.PG_SSL, "pgssl", "prefer", "disable / allow / prefer / require / verify-ca / verify-full - PostgreSQL ssl modes: https://www.postgresql.org/docs/current/libpq-ssl.html")
-
-/*
-	//escape test flags:
-	var _ = func() bool { 
-		testing.Init() 
-		return true 
-	}()
-*/
-
-	//process all flags
-	flag.Parse()
-
-	//делаем хеш от порта коллектора чтобы использовать в уникальном названии файла бд
-	config.COLLECTOR_LISTENER_ADDRESS_HASH = hash(config.COLLECTOR_LISTENER_ADDRESS)
-
-	//путь к файлу бд
-	config.DB_FULL_FILE_PATH = fmt.Sprintf(config.DB_FILE_PATH+"/"+config.APP_NAME+"."+config.COLLECTOR_LISTENER_ADDRESS_HASH+".db."+config.DB_TYPE)
-
-	if *flagVersion  {
-		if config.VERSION != "" {
-			fmt.Println("Version:", config.VERSION)
-		}
-		os.Exit(0)
+	type Settings struct {
+		APP_NAME, VERSION, COLLECTOR_LISTENER_ADDRESS, COLLECTOR_LISTENER_ADDRESS_HASH, WEB_LISTENER_ADDRESS, PROXY_ADDRESS, DB_TYPE, DB_FILE_PATH, DB_FULL_FILE_PATH, PG_HOST, PG_USER, PG_PASS, PG_DB_NAME, PG_SSL, TIME_ZONE string
+		PG_PORT int
+		AVERAGE_RESULTS bool
+		RACE_TIMEOUT_DURATION, MINIMAL_LAP_TIME_DURATION, AVERAGE_DURATION, DB_SAVE_INTERVAL_DURATION time.Duration
 	}
-	return
-}
+
+	func isFlagPassed(name string) bool {
+		found := false
+		flag.Visit(func(f *flag.Flag) {
+			if f.Name == name {
+				found = true
+			}
+		})
+		return found
+	}
+
+	func hash(s string) string {
+		hash := fnv.New32a()
+		hash.Write([]byte(s))
+		return fmt.Sprint(hash.Sum32())
+	}
+
+	func ParseFlags() (config Settings)  { 
+		config.APP_NAME = "chicha"
+		flagVersion := flag.Bool("version", false, "Output version information")
+		flag.StringVar(&config.COLLECTOR_LISTENER_ADDRESS, "collector", "0.0.0.0:4000", "Provide IP address and port to collect and parse data from RFID and timing readers.")
+		flag.StringVar(&config.WEB_LISTENER_ADDRESS, "web", "0.0.0.0:80", "Provide IP address and port to listen for HTTP connections from clients.")
+		flag.StringVar(&config.PROXY_ADDRESS, "proxy", "", "Proxy received data to another collector. For example: -proxy '10.9.8.7:4000'.")
+		flag.StringVar(&config.TIME_ZONE, "timezone", "UTC", "Set race timezone. Example: Europe/Paris, Africa/Dakar, UTC, https://en.wikipedia.org/wiki/List_of_tz_database_time_zones")
+		flag.DurationVar(&config.RACE_TIMEOUT_DURATION, "timeout", 2*time.Minute, "Set race timeout duration. After this time if nobody passes the finish line the race will be stopped. Valid time units are: 's' (second), 'm' (minute), 'h' (hour).")
+		flag.DurationVar(&config.MINIMAL_LAP_TIME_DURATION, "laptime", 45*time.Second, "Minimal lap time duration. Results smaller than this duration would be considered wrong." )
+		flag.DurationVar(&config.AVERAGE_DURATION, "average-duration", 1000*time.Millisecond, "Duration to calculate average results. Results passed to reader during this duration will be calculated as average result." )
+		flag.BoolVar(&config.AVERAGE_RESULTS, "average", true, "Calculate average results instead of only first results.")
+
+		//db
+		flag.StringVar(&config.DB_FILE_PATH, "dbpath", ".", "Provide path to writable directory to store database data.")
+		flag.StringVar(&config.DB_TYPE, "dbtype", "sqlite", "Select db type: sqlite / genji / postgres")
+		flag.DurationVar(&config.DB_SAVE_INTERVAL_DURATION, "db-save-interval", 30000*time.Millisecond, "Duration to save data from memory to database (disk). Setting duration too low may cause unpredictable performance results." )
+
+		//PostgreSQL related start
+		flag.StringVar(&config.PG_HOST, "pghost", "127.0.0.1", "PostgreSQL DB host.")
+		flag.IntVar(&config.PG_PORT, "pgport", 5432, "PostgreSQL DB port.")
+		flag.StringVar(&config.PG_USER, "pguser", "postgres", "PostgreSQL DB user.")
+		flag.StringVar(&config.PG_PASS, "pgpass", "", "PostgreSQL DB password.")
+		flag.StringVar(&config.PG_DB_NAME, "pgdbname", "chicha", "PostgreSQL DB name.")
+		flag.StringVar(&config.PG_SSL, "pgssl", "prefer", "disable / allow / prefer / require / verify-ca / verify-full - PostgreSQL ssl modes: https://www.postgresql.org/docs/current/libpq-ssl.html")
+
+		/*
+		//escape test flags:
+		var _ = func() bool { 
+			testing.Init() 
+			return true 
+		}()
+		*/
+
+		//process all flags
+		flag.Parse()
+
+		//делаем хеш от порта коллектора чтобы использовать в уникальном названии файла бд
+		config.COLLECTOR_LISTENER_ADDRESS_HASH = hash(config.COLLECTOR_LISTENER_ADDRESS)
+
+		//путь к файлу бд
+		config.DB_FULL_FILE_PATH = fmt.Sprintf(config.DB_FILE_PATH+"/"+config.APP_NAME+"."+config.COLLECTOR_LISTENER_ADDRESS_HASH+".db."+config.DB_TYPE)
+
+		if *flagVersion  {
+			if config.VERSION != "" {
+				fmt.Println("Version:", config.VERSION)
+			}
+			os.Exit(0)
+		}
+		return
+	}
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"fmt"
 	"time"
+	"strings"
 
 	"database/sql"
 
@@ -18,9 +19,9 @@ var respawnLock chan int
 var databaseWorkersMaxCount int = 1
 
 func Run(config Config.Settings) {
-	
-	//initialise channel with tasks:
-	DatabaseTask = make(chan Data.RawData)
+
+	//initialise channel with 1000000 tasks capacity:
+	DatabaseTask = make(chan Data.RawData, 1000000)
 
 	//initialize unblocking channel to guard respawn tasks
 	respawnLock = make(chan int, databaseWorkersMaxCount)
@@ -51,7 +52,7 @@ func deferCleanup(db *sql.DB) {
 }
 
 func databaseWorkerRun(workerId int, config Config.Settings ) {
-	
+
 
 	dbConnection, err := connectToDb(config)
 
@@ -66,17 +67,28 @@ func databaseWorkerRun(workerId int, config Config.Settings ) {
 	}
 
 	//initialise dbConnection error channel
-	connectionErrorChannel := make(chan error)
+	databaseErrorChannel := make(chan error)
 
 	go func() {
 		for {
-			_, err = dbConnection.Exec("UPDATE DBWatchDog SET UnixTime = ? WHERE ID = 1", time.Now().UnixMilli())
-			if err != nil {
-				connectionErrorChannel <- err
-				return
-			} else {
-				//log.Println("Database is alive.")
-				time.Sleep(1 * time.Second)
+			//do watchdog operations only if channel with database tasks is empty (channel length equals zero):
+			if len(DatabaseTask) == 0 {
+				_, err = dbConnection.Exec("UPDATE DBWatchDog SET UnixTime = ? WHERE ID = 1", time.Now().UnixMilli())
+				if err != nil {
+					//skip busy SQLITE database errors:
+					if !strings.Contains(err.Error(), "database is locked (5) (SQLITE_BUSY)") {
+						log.Println("Database watchdog error:", err)
+						databaseErrorChannel <- err
+						return
+					} else {
+						//sleep some time to calm down database operations:
+						log.Println("Watchdog notice: Database is busy - sleeping to calm down operations.")
+						time.Sleep(config.DB_SAVE_INTERVAL_DURATION)
+					}
+				} else {
+					//sleep some time to calm down database operations:
+					time.Sleep(config.DB_SAVE_INTERVAL_DURATION)
+				}
 			}
 		}
 	}()
@@ -84,18 +96,36 @@ func databaseWorkerRun(workerId int, config Config.Settings ) {
 	for {
 		select {
 			//в случае если есть задание в канале DatabaseTask
-		case currentDatabaseTask := <- DatabaseTask :
-			//log.Println("Received new database task with TagID:", currentDatabaseTask.TagID)
-			_, err := InsertRawDataInDB(dbConnection, currentDatabaseTask)
-			if err != nil {
-				log.Printf("Database worker %d exited due to processing error: %s\n", workerId, err)
-				return
+		case <- DatabaseTask :
+			//sleep some time to calm down disk operations:
+			time.Sleep(config.DB_SAVE_INTERVAL_DURATION)
+			//пробежать во всем доступным данным в канале заданий для бд и сохранить их в базе данных:
+			for currentDatabaseTask := range DatabaseTask {
+				_, err := InsertRawDataInDB(dbConnection, currentDatabaseTask)
+				if err != nil {
+					//skip busy SQLITE database errors:
+					if strings.Contains(err.Error(), "database is locked (5) (SQLITE_BUSY)") {
+						log.Println("Saving data to disk notice: Database is busy - sleeping to calm down operations.")
+						//return task to channel (this may lead to raw data id misorder in database):
+						DatabaseTask <- currentDatabaseTask
+						//sleep some time to calm down disk operations:
+						time.Sleep(config.DB_SAVE_INTERVAL_DURATION)
+					}  else {
+						//return task to channel (this may lead to raw data id misorder in database):
+						DatabaseTask <- currentDatabaseTask
+
+						log.Printf("Database worker %d exited due to critical processing error: %s\n", workerId, err)
+						return
+					}
+				}
 			}
-			//do come task:
-		case networkError := <-connectionErrorChannel :
-			//обнаружена сетевая ошибка - завершаем гоурутину
-			log.Printf("Database worker %d exited due to connection error: %s\n", workerId, networkError)
+			log.Println("Saved all data from memory to database.")
+		case databaseError := <-databaseErrorChannel :
+			//обнаружена критическая ошибка базы данных - завершаем гоурутину:
+			log.Printf("Database worker %d exited due to critical error: %s\n", workerId, databaseError)
 			return
+		default:
+			//set non blocking case
 		}
 	}
 }
